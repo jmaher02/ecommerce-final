7ef20e512c9be83d4dcc7d86ab337a1c5e6aecd9
jmaher02 <57075950+jmaher02@users.noreply.github.com>
2020-11-25 18:00:48 -0500

    Product details displaying on Product page

--- src/ECommerceController.java
+++ src/ECommerceController.java
@@ -23,13 +23,26 @@ public class ECommerceController
 	//Models
 	private Catalog catalog;
 	
+	@FXML private HBox titlePane;
+	
 	//Nodes
+	@FXML private Label title;
 	@FXML private Label productName;
 	@FXML private Label productPrice;
 	@FXML private Label productID;
 	@FXML private Label productDetails;
 	@FXML private TextField quantity;
+	
 	@FXML private Button addToCart;
+	@FXML private Button backButton;
+	@FXML private Button accountButton;
+	@FXML private Button cartButton;
+	//Button styling
+    private static final String IDLE_BUTTON_STYLE = "-fx-background-color: #B0C485";
+    private static final String HOVERED_BUTTON_STYLE = "-fx-background-color: #EBEFCC;";
+
+    //----FOR TESTING PRODUCTS  ---//
+    private int index = 0;
 	
 	@FXML private ImageView productImage;
 	
@@ -39,20 +52,59 @@ public class ECommerceController
 		catalog = new Catalog();
 		
 		//Update Label Text Colors
+		title.setTextFill(Color.web("FFFAEE"));
 		productName.setTextFill(Color.web("#689892"));
 		productDetails.setTextFill(Color.web("#8FB4A8"));
 		productID.setTextFill(Color.web("#8FB4A8"));
-		
+				
+		//Set default image
 		productImage.setImage(new Image("images/defaultProduct.png"));
 		
+		//Create hover style
+		setButtonHover(backButton);
+		setButtonHover(accountButton);
+		setButtonHover(addToCart);
+		setButtonHover(cartButton);
+	}
+	
+	@FXML
+	public void backToCategory( ActionEvent event)
+	{
+		System.out.println("GO BACK");
+	}
+	
+	@FXML void userSignIn( ActionEvent event)
+	{
+		System.out.println("ACCOUNT SCREEN");
 	}
 	
 	@FXML
 	public void updateCart(ActionEvent event)
-	{
+	{		
 		System.out.println("Pressed Button");
 	}
 	
+	@FXML
+	public void viewCart( ActionEvent event )
+	{
+		System.out.println("View Cart");
+	}
 	
+	public void setButtonHover( Button button )
+	{
+		button.setOnMouseEntered(e -> button.setStyle(HOVERED_BUTTON_STYLE));
+        button.setOnMouseExited(e -> button.setStyle(IDLE_BUTTON_STYLE));
+	}
+	
+	public void setProductScreen(Product product)
+	{
+		//Testing adding product details
+		productName.setText(product.getName());
+		productPrice.setText(product.printPrice());
+		productID.setText(product.getItemNumber() + "");
+		productDetails.setText(product.displayCharacteristics());
+		index++;
+		
+	}
 	
 }
